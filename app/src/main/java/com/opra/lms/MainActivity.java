76750025bd696d9d0d136a9ebeed17af66368647
Sangram <sangramharihar007@gmail.com>
2021-12-20 20:23:41 +0530

    Navigation added2

--- app/src/main/java/com/opra/lms/MainActivity.java
+++ app/src/main/java/com/opra/lms/MainActivity.java
@@ -2,9 +2,12 @@ package com.opra.lms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +18,15 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
-
         setContentView(R.layout.activity_main);
+        Button yourButton = (Button) findViewById(R.id.btn_signup);
+
+        yourButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, home.class));
+
+            }
+        });
     }
 }
